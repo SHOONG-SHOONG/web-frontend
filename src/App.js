b34a3251ec66ef6c3dea12c4cae387843f0a59c7
DoomchitYJ <poj1004@inha.edu>
2025-05-21 10:58:12 +0900

    feat: 장바구니 페이지 구현

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 // 페이지 컴포넌트 임포트
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./main/page.tsx";
-import ItemPage from "./item/page.tsx";
+// import OrderPage from "./order/page.tsx";
+import CartPage from "./cart/page.tsx";
 // import Home from "./pages/Home.js";
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/item" element={<ItemPage />} />
+        {/* <Route path="/item" element={<ItemPage />} /> */}
+        {/* <Route path="/order" element={<OrderPage />} /> */}
+        <Route path="/cart" element={<CartPage />} />
         {/* <Route path="/" element={<Home />} /> */}
       </Routes>
     </BrowserRouter>
