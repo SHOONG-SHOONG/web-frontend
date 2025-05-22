51bbb196653983b7d7dc8c556d6703c9347cfb02
wns5120 <ghwns7171@naver.com>
2025-05-22 10:25:31 +0900

    검색페이지 데모버전

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./main/page.tsx";
 import OrderPage from "./order/page.tsx";
 import CartPage from "./cart/page.tsx";
+import ProductPage from "./pages/search/Search.js";
 // import Home from "./pages/Home.js";
 
 function App() {
@@ -13,9 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         {/* <Route path="/item" element={<ItemPage />} /> */}
-        <Route path="/order" element={<OrderPage />} />
-        <Route path="/cart" element={<CartPage />} />
+        {/* <Route path="/order" element={<OrderPage />} />
+        <Route path="/cart" element={<CartPage />} /> */}
         {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/item/search" element={<ProductPage />} />
       </Routes>
     </BrowserRouter>
   );
