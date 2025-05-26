6e58e3be9c3599c594198c68a276a2a3b18a91af
DoomchitYJ <poj1004@inha.edu>
2025-05-26 20:16:45 +0900

    feat: Login, 404 Error 페이지에 Header, Footer 추가

--- src/apps/error/NotFoundPage.tsx
+++ src/apps/error/NotFoundPage.tsx
@@ -3,6 +3,8 @@ import { Button, Container, Text, Title, Stack, Center } from "@mantine/core";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
+import HeaderComponent from "../../components/Header.tsx";
+import FooterComponent from "../../components/Footer.tsx";
 
 export const PlaneEmoji = () => (
   <motion.span
@@ -41,23 +43,30 @@ export default function NotFoundPage() {
   const navigate = useNavigate();
 
   return (
-    <Container size="sm" py={80}>
-      <Center>
-        <Stack align="center">
-          <Text fz={64}>
-            <PlaneEmoji /> <CloudEmoji /> <LuggageEmoji />
-          </Text>
-          <Title order={2}>요청하신 페이지를 찾을 수 없습니다</Title>
-          <Text ta="center" c="dimmed">
-            주소가 잘못되었거나
-            <br />
-            여행지처럼 사라진 페이지예요.
-          </Text>
-          <Button onClick={() => navigate("/")} variant="outline" color="black">
-            메인으로 돌아가기
-          </Button>
-        </Stack>
-      </Center>
-    </Container>
+    <>
+      {/* Header */}
+      <HeaderComponent />
+      <Container size="sm" py={80}>
+        <Center>
+          <Stack align="center">
+            <Text fz={64}>
+              <PlaneEmoji /> <CloudEmoji /> <LuggageEmoji />
+            </Text>
+            <Title order={2}>요청하신 페이지를 찾을 수 없습니다</Title>
+            <Text ta="center" c="dimmed">
+              주소가 잘못되었거나
+              <br />
+              여행지처럼 사라진 페이지예요.
+            </Text>
+            <Button onClick={() => navigate("/")} variant="outline" color="black">
+              메인으로 돌아가기
+            </Button>
+          </Stack>
+        </Center>
+      </Container>
+
+      {/* Footer */}
+      <FooterComponent />
+    </>
   );
 }
